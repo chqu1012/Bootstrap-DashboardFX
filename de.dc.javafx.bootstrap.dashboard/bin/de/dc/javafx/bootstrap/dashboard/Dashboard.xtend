1600d63f681e671deba8fe48dc5fee330cb0e8ff
Dim <cqd2202@yahoo.de>
2019-06-07 11:15:14 +0200

    BTDB-00006: Changed root panel to anchorpane

--- de.dc.javafx.bootstrap.dashboard/bin/de/dc/javafx/bootstrap/dashboard/Dashboard.xtend
+++ de.dc.javafx.bootstrap.dashboard/bin/de/dc/javafx/bootstrap/dashboard/Dashboard.xtend
@@ -1,22 +1,33 @@
 package de.dc.javafx.bootstrap.dashboard
 
+import de.dc.javafx.bootstrap.dashboard.controller.IndexPage
 import de.dc.javafx.bootstrap.dashboard.controller.LoginPage
+import de.dc.javafx.bootstrap.dashboard.controller.RegisterPage
 import javafx.application.Application
 import javafx.scene.Scene
+import javafx.scene.layout.AnchorPane
+import javafx.scene.layout.Pane
 import javafx.stage.Stage
-import javafx.scene.layout.StackPane
-import de.dc.javafx.bootstrap.dashboard.controller.RegisterPage
-import de.dc.javafx.bootstrap.dashboard.controller.IndexPage
 
 class Dashboard extends Application {
 	
 	override start(Stage stage) throws Exception {
-		val pane = new StackPane
-		pane.children += new RegisterPage
-		pane.children += new IndexPage
-		pane.children += new LoginPage
+		val pane = new AnchorPane
+		
+		pane.children += anchor(new RegisterPage)
+		pane.children += anchor(new IndexPage)
+		pane.children += anchor(new LoginPage)
+		
 		stage.setScene(new Scene(pane, 1200, 700))
 		stage.show
+	}
+	
+	def static anchor(Pane pane){
+		AnchorPane.setBottomAnchor(pane, 0d)
+		AnchorPane.setTopAnchor(pane, 0d)
+		AnchorPane.setLeftAnchor(pane, 0d)
+		AnchorPane.setRightAnchor(pane, 0d)
+		return pane
 	}
 
 	def static void main(String[] args) {
--- de.dc.javafx.bootstrap.dashboard/xtend-gen/de/dc/javafx/bootstrap/dashboard/Dashboard.java
+++ de.dc.javafx.bootstrap.dashboard/xtend-gen/de/dc/javafx/bootstrap/dashboard/Dashboard.java
@@ -7,26 +7,38 @@ import javafx.application.Application;
 import javafx.collections.ObservableList;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 @SuppressWarnings("all")
 public class Dashboard extends Application {
   @Override
   public void start(final Stage stage) throws Exception {
-    final StackPane pane = new StackPane();
+    final AnchorPane pane = new AnchorPane();
     ObservableList<Node> _children = pane.getChildren();
     RegisterPage _registerPage = new RegisterPage();
-    _children.add(_registerPage);
+    Pane _anchor = Dashboard.anchor(_registerPage);
+    _children.add(_anchor);
     ObservableList<Node> _children_1 = pane.getChildren();
     IndexPage _indexPage = new IndexPage();
-    _children_1.add(_indexPage);
+    Pane _anchor_1 = Dashboard.anchor(_indexPage);
+    _children_1.add(_anchor_1);
     ObservableList<Node> _children_2 = pane.getChildren();
     LoginPage _loginPage = new LoginPage();
-    _children_2.add(_loginPage);
+    Pane _anchor_2 = Dashboard.anchor(_loginPage);
+    _children_2.add(_anchor_2);
     Scene _scene = new Scene(pane, 1200, 700);
     stage.setScene(_scene);
     stage.show();
+  }
+  
+  public static Pane anchor(final Pane pane) {
+    AnchorPane.setBottomAnchor(pane, Double.valueOf(0d));
+    AnchorPane.setTopAnchor(pane, Double.valueOf(0d));
+    AnchorPane.setLeftAnchor(pane, Double.valueOf(0d));
+    AnchorPane.setRightAnchor(pane, Double.valueOf(0d));
+    return pane;
   }
   
   public static void main(final String[] args) {
